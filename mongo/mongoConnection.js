7fdaf0815414ec56bcb732ad764a4def645c6ca0
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2016-01-28 16:34:33 -0500

    mongo recipes

--- mongo/mongoConnection.js
+++ mongo/mongoConnection.js
@@ -4,11 +4,13 @@ mongoose.Promise = require('bluebird');
 var debug = require('debug')('mongo:connection');
 
 if(!process.env.hasOwnProperty('MONGOURI')){
+  console.error('Need MONGOURI environment variable');
+  console.error('mongodb://<dbuser>:<dbpassword>@<host>:<port>/<dbname>');
   throw new Error('MONGOURI environment variable must be set!!');
 }
 
 //connect
-debug('attempting to connect to ', mongoURI);
+debug('attempting to connect to ', process.env.MONGOURI);
 mongoose.connect(process.env.MONGOURI);
 
 //get DB event emitter to handle connection events
